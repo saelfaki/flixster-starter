3cbf561bd7bcb7176a2d677bd378832b53c71d31
Saja Elfaki <saja@meta.com>
2024-06-14 14:27:03 -0700

    adding search/now playing and modal to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import MovieList from './MovieList'
+import Search from './Search'
+import MovieModal from './MovieModal';
 
 
 
@@ -8,6 +10,14 @@ import MovieList from './MovieList'
 const App = () => {
   const[movies, setMovies] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
+  const [input, setInput] = useState('');
+  const [OpenSearch, setOpenSearch] = useState(false);
+  const [nowPlaying, setnowPlaying] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [url, setUrl] = useState(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${pageNumber}`);
+
+
+
 
 
 
@@ -38,6 +48,50 @@ const App = () => {
     setUrl(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${pageNumber+1}`);
   }
 
+  function handleSearchPage(){
+    setOpenSearch(true);
+    setnowPlaying(false);
+  }
+
+
+  function handleSearchInput(userSearch){
+    setMovies([]);
+    setOpenSearch(true);
+    setnowPlaying(false);
+    setUrl(`https://api.themoviedb.org/3/search/movie?query=${userSearch}&include_adult=false&language=en-US&page=1`);
+  }
+
+
+  function emptySearchBar(){
+    setMovies([]);
+    setUrl(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${pageNumber}`);
+  }
+
+
+
+  function handlenowPlaying(){
+  setnowPlaying(true);
+  setOpenSearch(false);
+  setPageNumber(1);
+  setUrl(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${pageNumber}`);
+  }
+
+  const openModal = (id) => {
+    setShowModal(true);
+    setSelectedMovieID(id);
+  };
+
+
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+
+
+
+
+
 
 
 
@@ -47,11 +101,41 @@ return(
     <div className="App">
     <header className="App-header">
       <h1>Flixster</h1>
+      <div className="nowplayingButton">
+        <button onClick={handlenowPlaying}>Now Playing</button>
+       </div>
+       <div className='searchButton'>
+         <button onClick={handleSearchPage}>Search</button>
+       </div>
+
     </header>
+
     <main>
 
-    <MovieList displayModal={openModal} movieData={movies} favorited={handleFavoritedMovies} watched={handleWatchedMovies}/>
-    <button className="loadmore"onClick={loadButton}>Load More</button>
+
+
+   {nowPlaying ?
+   <>
+
+
+     {showModal ? <MovieModal id={selectedMovieID} data={movies} closeModal={closeModal}/> : null}
+
+
+     <MovieList displayModal={openModal} movieData={movies} favorited={handleFavoritedMovies} watched={handleWatchedMovies}/>
+     <button className="loadmore"onClick={loadButton}>Load More</button>
+   </> :
+   <>
+
+
+
+
+   <Search input={input} search={handleSearchInput} emptySearchBar={emptySearchBar}/>
+   <MovieList movieData={movies} watched={handleWatchedMovies} favorited={handleFavoritedMovies}/>
+   </>
+
+
+   }
+
 
 
     </main>
